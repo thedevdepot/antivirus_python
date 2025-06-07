fa897b386b07b33b631f889493ada90b4c965d8a
dev <tonyringertx@gmail.com>
2025-06-07 18:27:18 -0500

    fixed the file path issue

--- detect_file.py
+++ detect_file.py
@@ -1,25 +1,22 @@
 import os
 
-print("UT1 complete")
-
-def isFileMalicious(file_path, malicious_signature): 
+def is_file_malicious(file_path, malicious_signature): 
     try:
         with open(file_path, "rb") as file:
-            print("UT2 complete")
             fileContent = file.read()
             if malicious_signature in fileContent:
                 return True
             return False
     except FileNotFoundError:
-        print("File not found")
+        print(f"File not found: {file_path}")
         return False
 
 
-if __name__ == "__main":
+if __name__ == "__main__":
     malicious_signature = b"tH1s_!ZaT36T"
-    file_path = r"/signatures/test_signature.txt"
+    file_path = r"./signatures/test_signature.txt"
 
-    if isFileMalicious(file_path, malicious_signature):
+    if is_file_malicious(file_path, malicious_signature):
         print("Potential malicious file detected")
     else:
         print("No malicious file detected")
